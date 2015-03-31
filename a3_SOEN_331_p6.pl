7ab9a66786fdc005f73df78473aece1aab9a3ee0
Aymangt <aymalsh@gmail.com>
2015-03-30 20:00:06 -0400

    pl update + spec part 4&5

--- a3_SOEN_331_p6.pl
+++ a3_SOEN_331_p6.pl
@@ -39,10 +39,10 @@ transition(init,error_diagnosis,init_crash,no_guard,init_err_msg).%check the act
 transition(idle,monitoring,begin_monitoring,no_guard,null).
 transition(idle,eroor_diagnosis,idle_Crash,no_guard,idle_err_msg).%check the action
 transition(monitoring,error_diagnosis,monitor_crash,no_guard,monitor_err_msg).%check the action
-transition(error_diagnosis,safe_shutdown,shutdown,retry>2,retry=0).%how to change variable value
+transition(error_diagnosis,safe_shutdown,shutdown,Retry>2,Retry=0).%how to change variable value
 transition(error_diagnosis,monitoring,moni_rescue,no_guard,null).%what’s the guard
 transition(error_diagnosis,idle,idle_rescue,no_guard,null).%what’s the guard
-transition(error_diagnosis,init,retry_init,retry<2,retry+1).%how to change variable value
+transition(error_diagnosis,init,retry_init,Retry<2,Retry+1).%how to change variable value
 transition(safe_shutdown,dormant,sleep,no_guard,null).
 %---------------------------
 
@@ -81,13 +81,12 @@ superstate(monitoring,lockdown).
 initial_state(monidle,monitoring).
 
 transition(monidle,regulate_invironment,no_contagion,no_guard,null).
-transition(monidle,lockdown,contagion_alert,no_guard,[inlockdown=true,FACILITY_CRIT_MESG]). %check variable value change and action msg+ compound actions
+transition(monidle,lockdown,contagion_alert,no_guard,'Inlockdown=true,FACILITY_CRIT_MESG'). %check variable value change and action msg
 transition(regulate_invironment,monidle,after_100ms,no_guard,null).
-transition(lockdown,monidle,purge_succ,no_guard,inlockdown=false).%check variable value change
+transition(lockdown,monidle,purge_succ,no_guard,Inlockdown=false).%check variable value change
 %----------------------------
 
 %under lockdown
-%check concurrent states
 
 state(prep_vpurge).
 state(alt_temp).
@@ -98,7 +97,7 @@ state(safe_status).
 superstate(lockdown,prep_vpurge).
 superstate(lockdown,alt_temp).
 superstate(lockdown,alt_psi).
-superstate(lockdown,resi_assess).
+superstate(lockdown,risk_assess).
 superstate(lockdown,safe_status).
 
 initial_state(prep_vpurge,lockdown).
@@ -107,8 +106,8 @@ transition(prep_vpurge,alt_temp,initiate_purge,no_guard,lockdoors).
 transition(prep_vpurge,alt_psi,initiate_purge,no_guard,lockdoors).
 transition(alt_temp,risk_assess,tcyc_comp,no_guard,null).
 transition(alt_psi,risk_assess,psicyc_comp,no_guard,null).
-transition(risk_assess,prep_vpurge,no_event,risk>0.01,null).
-transition(risk_assess,safe_status,no_event,risk<0.01,unlock_doors).
+transition(risk_assess,prep_vpurge,no_event,Risk>0.01,null).
+transition(risk_assess,safe_status,no_event,Risk<0.01,unlock_doors).
 %----------------------------
 
 %under error diagnosis
@@ -123,12 +122,10 @@ superstate(error_diagnosis,reset_module_data).
 
 initial_state(error_rcv,error_diagnosis).
 
-transition(error_rcv,applicable_rescue,no_event,err_protocol_def=true,null).
-transition(error_rcv,reset_module_data,no_event,err_protocol_def=false,null).
+transition(error_rcv,applicable_rescue,no_event,Err_protocol_def=true,null).
+transition(error_rcv,reset_module_data,no_event,Err_protocol_def=false,null).
 transition(applicable_rescue,exit,apply_protocol_rescue,no_guard,null).%a transition to exit
 transition(reset_module_data,exit,reset_to_stable,no_guard,null).%a transition to exit
-transition(s1,s1,event1,guard1,hi).%a loop edge for testing only
-transition(s2,s2,event2,guard2,null).%a loop edge for testing only
 %--------------------------------
 is_loop(Event,Guard):-
 	transition(State,State,Event,Guard,_).
@@ -149,11 +146,33 @@ all_superstates(Set):-
 	list_to_set(L,Set).	
 ancestor(Ancestor,Descendant):-
 	superstate(Ancestor,Descendant).
+inherits_transitions(State, List):-
+	superstate(Super,State),
+	findall([Super,Distination,Event,Gurad,Action],transition(Super,Distination,Event,Gurad,Action),List).
 all_states(L):-
-	findall(State,state(State),List),
-	list_to_set(List,L).%a set is return not a list
+	findall(State,state(State),L).
 all_init_states(L):-
-	findall(State,initial_state(State,_),List),
-	list_to_set(List,L).%a set is return not a list
+	findall(State,initial_state(State,_),L).
 get_starting_state(State):-
 	initial_state(State,null).
+state_is_reflexive(State):-
+	transition(State,State,_,_,_).
+graph_is_reflexive:-
+	state(State),
+	state_is_reflexive(State).	
+get_guards(Set):-
+	findall(Guard,transition(_,_,_,Guard,_),L),
+	list_to_set(L,Set).
+get_events(Set):-
+	findall(Event,transition(_,_,Event,_,_),L),
+	list_to_set(L,Set).
+get_actions(Set):-
+	findall(Action,transition(_,_,_,_,Action),L),
+	list_to_set(L,Set).
+get_only_guarded(Set):-
+	findall([State1,State2],(transition(State1,State2,no_event,Guard,null),Guard\=no_guard),L),%actions are null or _
+	list_to_set(L,Set).
+legal_events_of(State,L):-
+	findall([Event,Guard],(transition(State,_,Event,Guard,_),(Event\=no_event;Guard\=no_guard)),List),
+	list_to_set(List,L).
+
